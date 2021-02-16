7b94c236674ec5820760f2aadb1feefd3228868a
shiam <shiamabdullah1@gmail.com>
2021-02-17 00:34:14 +0600

    4a done

---
+++ MID_LAB_TASK_PHP_05/TASK_4/TASK_A/gender.php
@@ -0,0 +1,16 @@
+<?php 
+
+if (isset($_POST['submit'])){
+    $gender= $_POST['gender'];
+
+    if($gender==""){
+        echo "Null submission----- ";
+    }else
+    {
+        echo "Success" ;  }
+}
+else{
+    echo "Invalid Request-----";
+}
+
+?>
