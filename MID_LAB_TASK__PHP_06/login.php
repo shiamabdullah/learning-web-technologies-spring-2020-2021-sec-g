7e2e120c16e16e3eaea5dbd8de5b9cdb1768bc8e
shiam <shiamabdullah1@gmail.com>
2021-02-23 09:22:42 +0600

    hyperlink

--- MID_LAB_TASK__PHP_06/login.php
+++ MID_LAB_TASK__PHP_06/login.php
@@ -45,7 +45,7 @@ if(isset($_POST['submit'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>LOGIN</title>
 </head>
 <body>
     <form method="POST" action="#">
@@ -66,7 +66,7 @@ if(isset($_POST['submit'])){
                     <input type="checkbox" name="item[]" >Remember Me <br> <br>
 
                     <input type="submit" name="submit" value="Submit">
-                    <a href=" https://www.google.com/">Forgot Password?</a> <br>
+                    <a href="changepass.php">Forgot Password?</a> <br>
 
         </fieldset>
     </form>
