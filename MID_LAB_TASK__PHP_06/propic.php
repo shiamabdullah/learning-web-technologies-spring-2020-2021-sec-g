ed3ecbdef6f1a4d32c60dc74b2ea0e721044f08b
shiam <shiamabdullah1@gmail.com>
2021-02-23 09:36:32 +0600

    task 3 unfinished

---
+++ MID_LAB_TASK__PHP_06/propic.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pp</title>
+</head>
+<body>
+    <form method="POST" action="#">
+        <fieldset>
+            <legend>PROFILE PICTURE</legend>
+                <table>
+                <td>  
+                    <input type="file" name="image" value="">
+                </td>
+        </table>        
+    </fieldset>
+
+        
+    </form>
+    
+</body>
+</html>
