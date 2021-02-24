52e60d85fe6251311c49fcd156571059ede9812f
shiam <shiamabdullah1@gmail.com>
2021-02-25 01:19:17 +0600

    task 4 finalized

--- MID_LAB_TASK__PHP_06/form.php
+++ MID_LAB_TASK__PHP_06/form.php
@@ -53,13 +53,13 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Registration</title>
 </head>
 
 <body>
     <form method="POST" action="#">
         <fieldset style="width:320px">
-            <legend>Registration</legend>
+            <legend><h3>REGISTRATION</h3></legend>
             <table>
 
                 <tr height='30px'>
