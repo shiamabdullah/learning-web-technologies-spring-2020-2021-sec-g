40c0dc5f73ceb39b5ce2e253b3cf7550a7fea44d
shiam <shiamabdullah1@gmail.com>
2021-03-23 08:50:29 +0600

    add product view done

---
+++ FINAL_LAB_TASK_02_PHP/model/productModel.php
@@ -0,0 +1,26 @@
+<?php
+	
+require_once('db.php');
+
+
+function addProduct($product){
+	$conn = getConnection();
+	$name= $product['name'];
+	$buying_price= $product['buying_price'];
+	$selling_price= $product['selling_price'];
+	$displayable= $product['displayable'];
+
+	$sql = "INSERT INTO `products` (`id`, `name`, `buying_price`, `selling_price`, `displayable`) VALUES (NULL, '{$name}', '{$buying_price}', '{$selling_price}', '{$displayable}');";
+	$result = mysqli_query($conn, $sql);
+
+	if($result){
+		return true;
+	}else{
+		return false;
+	}
+
+}
+
+
+
+?>
